fed4f2e228ffdf4ac8a93e174626af8a3c8ffb5d
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-10 17:01:25 -0300

    finalize mechanism

--- main/main.ino
+++ main/main.ino
@@ -7,9 +7,15 @@ Servo space;
 
 void setup() {
   space.attach(9);
+  space.write(20);
 }
 
 void loop() {
   float ldr = analogRead(LDR);
-  
+  if(ldr > LIM){
+    space.write(20);
+    delay(150);
+  }
+  space.write(0);
+  delay(50);
 }
